025bb01d5693cdd5abdff26391a32ebf6f86ec4d
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-04-13 16:05:37 +0000

    Update package versions

--- dataloader/base.py
+++ dataloader/base.py
@@ -3,4 +3,6 @@ import random
 
 
 class AbstractDataloader(metaclass=ABCMeta):
-    def __init__(self, args,
+    def __init__(self, args, dataset):
+        self.args = args
+        self.save_folder = dataset._get_
