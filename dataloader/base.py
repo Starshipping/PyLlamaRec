99e7f83d40df46f8f97271cda697af8026765d65
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-04-14 10:42:03 +0000

    deploying

--- dataloader/base.py
+++ dataloader/base.py
@@ -5,4 +5,6 @@ import random
 class AbstractDataloader(metaclass=ABCMeta):
     def __init__(self, args, dataset):
         self.args = args
-        self.save_folder = dataset._get_
+        self.save_folder = dataset._get_preprocessed_folder_path()
+        dataset = dataset.load_dataset()
+        sel
