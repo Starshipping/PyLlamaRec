ea4623633f3c4b9135a5ea189d79ed0e66cb53b4
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-05-09 20:08:33 +0000

    re-run make idl.

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -40,4 +40,5 @@ def generate_and_tokenize_train(args, data_point, tokenizer, prompter):
                            max_length=args.llm_max_text_len,
                            padding=False,
                            return_tensors=None)
-        if (result["input_ids"][-1] != tokenizer.eos_token_id and add_eos_t
+        if (result["input_ids"][-1] != tokenizer.eos_token_id and add_eos_token):
+            result["input_ids"].append(tokenizer.eos_token_id
