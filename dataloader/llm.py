82d35bf98393818b80f6a6f9da8109c29058e90e
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-05-26 13:44:52 +0000

    gravity

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -108,4 +108,5 @@ class LLMDataloader():
         
         self.llm_retrieved_path = args.llm_retrieved_path
         print('Loading retrieved file from {}'.format(self.llm_retrieved_path))
-        retrieved_file = pickle
+        retrieved_file = pickle.load(open(os.path.join(args.llm_retrieved_path,
+                       
