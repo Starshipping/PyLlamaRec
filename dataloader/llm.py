1038ac33727b92c3ea12fc0e234f4d514cfe224a
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-06-13 12:18:40 +0000

    ext4: fix BUG_ON in mb_free_blocks()

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -160,4 +160,7 @@ class LLMDataloader():
         dataset = self._get_train_dataset()
         dataloader = data_utils.DataLoader(dataset, batch_size=self.args.lora_micro_batch_size,
                                            shuffle=True, pin_memory=True, num_workers=self.args.num_workers,
-                                    
+                                           worker_init_fn=worker_init_fn)
+        return dataloader
+
+    def _get_
