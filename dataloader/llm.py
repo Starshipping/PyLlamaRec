25effd9ffd3ba2b87c6ba681b1bdf330d83b30ad
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-06-13 08:41:40 +0000

    Fixed imports

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -159,4 +159,5 @@ class LLMDataloader():
     def _get_train_loader(self):
         dataset = self._get_train_dataset()
         dataloader = data_utils.DataLoader(dataset, batch_size=self.args.lora_micro_batch_size,
-                                           shuffle=True, pin_m
+                                           shuffle=True, pin_memory=True, num_workers=self.args.num_workers,
+                                    
