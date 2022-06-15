96814e49648332be9ae498280ea2fd69c21805a6
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-06-15 19:38:48 +0000

    better build

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -177,4 +177,5 @@ class LLMDataloader():
     def _get_eval_loader(self, mode):
         batch_size = self.args.val_batch_size if mode == 'val' else self.args.test_batch_size
         dataset = self._get_eval_dataset(mode)
-        dataloader = data_utils.DataLoader(dataset, batch_size=
+        dataloader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=True,
+                                           pin_memory
