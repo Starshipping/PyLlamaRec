91ce620d55b3e2f7eac550bc00bdda9f0f49e252
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-06-16 06:02:42 +0000

    Adding generated files

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -178,4 +178,7 @@ class LLMDataloader():
         batch_size = self.args.val_batch_size if mode == 'val' else self.args.test_batch_size
         dataset = self._get_eval_dataset(mode)
         dataloader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=True,
-                                           pin_memory
+                                           pin_memory=True, num_workers=self.args.num_workers)
+        return dataloader
+
+    def _get_eval_da
