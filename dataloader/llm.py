edb69849d2f62ffedd030a51211db1bf0cdb4c68
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-06-17 06:04:34 +0000

    Import translations. DO NOT MERGE ANYWHERE

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -184,4 +184,5 @@ class LLMDataloader():
     def _get_eval_dataset(self, mode):
         if mode == 'val':
             dataset = LLMValidDataset(self.args, self.train, self.val, self.max_len, self.rng, \
-                                      self.text_dict, self.tokenizer, self.p
+                                      self.text_dict, self.tokenizer, self.prompter, self.val_users, \
+                                      self.val_candida
