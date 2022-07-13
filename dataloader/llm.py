6001e765ad1c5c9c323e227f0810ca10e8597ded
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-07-13 16:41:49 +0000

    Added changelog file

--- dataloader/llm.py
+++ dataloader/llm.py
@@ -289,4 +289,4 @@ class LLMTestDataset(data_utils.Dataset):
         assert answer in candidates
         # self.rng.shuffle(candidates)
 
-        return seq_to_token_ids(self.args, seq, candidates, answer, self.text_dict, se
+        return seq_to_token_ids(self.args, seq, candidates, answer, self.text_dict, self.tokenizer, self.prompter, eval=True)
