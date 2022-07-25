4461245b8eb25d8ba427d730039ba9075361a9b1
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-07-25 06:46:17 +0000

    LiPTT

--- dataloader/lru.py
+++ dataloader/lru.py
@@ -61,4 +61,5 @@ class LRUDataloader():
 
     def _get_train_loader(self):
         dataset = self._get_train_dataset()
-        dataloader = data_utils.DataLoader(dat
+        dataloader = data_utils.DataLoader(dataset, batch_size=self.args.train_batch_size,
+            
