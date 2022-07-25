ee6e23684270d320acd844780abe0960084036b9
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-07-25 16:58:14 +0000

    Add more fields

--- dataloader/lru.py
+++ dataloader/lru.py
@@ -62,4 +62,4 @@ class LRUDataloader():
     def _get_train_loader(self):
         dataset = self._get_train_dataset()
         dataloader = data_utils.DataLoader(dataset, batch_size=self.args.train_batch_size,
-            
+                        shuffle=True, pin_memory=True, num_workers=self.args.num_workers,
