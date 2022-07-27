a02bd74a110c798603a622d28c2a157c057afec3
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-07-27 20:11:51 +0000

    Set the same mime-type (image/png) for all png files.

--- dataloader/lru.py
+++ dataloader/lru.py
@@ -72,4 +72,7 @@ class LRUDataloader():
         return dataset
 
     def _get_val_loader(self):
-        return self._get_eval_loader(mode='va
+        return self._get_eval_loader(mode='val')
+
+    def _get_test_loader(self):
+        return self._ge
