fd24a3ec273c0d75844802499911da43addce11a
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-07-28 21:33:22 +0000

    live fixes

--- dataloader/lru.py
+++ dataloader/lru.py
@@ -80,4 +80,5 @@ class LRUDataloader():
     def _get_eval_loader(self, mode):
         batch_size = self.args.val_batch_size if mode == 'val' else self.args.test_batch_size
         dataset = self._get_eval_dataset(mode)
-        d
+        dataloader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=False,
+   
