36b8d042b6f0c682e89ffe4e10946361d47f0a0f
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-09-21 11:12:55 +0000

    Improved comment

--- datasets/base.py
+++ datasets/base.py
@@ -1,3 +1,68 @@
 
 import pickle
 import shutil
+import tempfile
+import os
+from pathlib import Path
+import gzip
+from abc import *
+from .utils import *
+from config import RAW_DATASET_ROOT_FOLDER
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
+
+
+class AbstractDataset(metaclass=ABCMeta):
+    def __init__(self, args):
+        self.args = args
+        self.min_rating = args.min_rating
+        self.min_uc = args.min_uc
+        self.min_sc = args.min_sc
+
+        assert self.min_uc >= 2, 'Need at least 2 ratings per user for validation and test'
+
+    @classmethod
+    @abstractmethod
+    def code(cls):
+        pass
+
+    @classmethod
+    def raw_code(cls):
+        return cls.code()
+
+    @classmethod
+    def zip_file_content_is_folder(cls):
+        return True
+
+    @classmethod
+    def all_raw_file_names(cls):
+        return []
+
+    @classmethod
+    @abstractmethod
+    def url(cls):
+        pass
+
+    @abstractmethod
+    def preprocess(self):
+        pass
+
+    @abstractmethod
+    def load_ratings_df(self):
+        pass
+
+    @abstractmethod
+    def maybe_download_raw_dataset(self):
+        pass
+
+    def load_dataset(self):
+        self.preprocess()
+        dataset_path = self._get_preprocessed_dataset_path()
+        dataset = pickle.load(dataset_path.open('rb'))
+        return dataset
+
+    def filter_triplets(self, df):
+        print('Filtering triplets')
