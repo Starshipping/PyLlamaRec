2b837bed12e1cb16af6b3bb281f809fc4107b737
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-10-12 20:40:36 +0000

    Better logs

--- model/lru.py
+++ model/lru.py
@@ -74,4 +74,5 @@ class LRUModel(nn.Module):
         # left padding to the power of 2
         seq_len = x.size(1)
         log2_L = int(np.ceil(np.log2(seq_len)))
-        x = F.pad(x, (0, 0, 2 ** log2_L - x.size(1), 0, 
+        x = F.pad(x, (0, 0, 2 ** log2_L - x.size(1), 0, 0, 0))
+        mask_ = F.pad(mask, (2 ** log2_L - mask.size(1), 0
