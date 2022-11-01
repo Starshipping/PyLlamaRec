df10715f79cca6a2e4a24dafa840f4b52cc0f681
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-11-01 15:00:50 +0000

    set up travis

--- model/lru.py
+++ model/lru.py
@@ -155,4 +155,5 @@ class LRULayer(nn.Module):
         # compute h in parallel
         log2_L = int(np.ceil(np.log2(h.size(1))))
         B, L, D = h.size(0), h.size(1), h.size(2)
-        
+        for i in range(log2_L):
+            h, lamb = self.lru_parallel(i +
