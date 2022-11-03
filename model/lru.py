cfdb5efbf625e51426459de9d53ab4b2a62bdaae
Starshipping <138149411+Starshipping@users.noreply.github.com>
2022-11-03 14:01:05 +0000

    Fixed a thing

--- model/lru.py
+++ model/lru.py
@@ -165,4 +165,6 @@ class PositionwiseFeedForward(nn.Module):
     def __init__(self, d_model, d_ff, dropout=0.1):
         super().__init__()
         self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Line
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.activation = nn.GELU()
+        self.dropout = n
