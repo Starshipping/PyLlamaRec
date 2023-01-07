bd1bcbcc639572e9210850d9323cfb88f9e7bc63
Starshipping <138149411+Starshipping@users.noreply.github.com>
2023-01-07 20:11:13 +0000

    prj

--- train_ranker.py
+++ train_ranker.py
@@ -59,4 +59,10 @@ def main(args, export_root=None):
     model.print_trainable_parameters()
 
     model.config.use_cache = False
-    trainer = LLMTrainer(args, model, train_loader, val_loader, test_loader, tokenizer, export_root, args.u
+    trainer = LLMTrainer(args, model, train_loader, val_loader, test_loader, tokenizer, export_root, args.use_wandb)
+    
+    trainer.train()
+    trainer.test(test_retrieval)
+
+
+if __name__ 
