052dde7d5da741dbdeb4d573db7760b580e3e5a3
Starshipping <138149411+Starshipping@users.noreply.github.com>
2023-02-23 14:06:45 +0000

    Bump eslint from 7.10.0 to 7.11.0

--- trainer/lru.py
+++ trainer/lru.py
@@ -65,4 +65,7 @@ class LRUTrainer(BaseTrainer):
             print('****************** Generating Candidates for Test Set ******************')
             tqdm_dataloader = tqdm(self.test_loader)
             for batch_idx, batch in enumerate(tqdm_dataloader):
-                
+                batch = self.to_device(batch)
+                seqs, labels = batch
+        
+    
